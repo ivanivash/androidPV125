0d4348cd0ce35eebfb57892ed02f6dbb4d086150
ivanivash <ivanivash20@gmail.com>
2023-10-18 20:16:49 +0300

    image

--- WebKovbasa/WebKovbasa/Program.cs
+++ WebKovbasa/WebKovbasa/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var dir = Path.Combine(Directory.GetCurrentDirectory(), "images");
+
+if(!Directory.Exists(dir))
+{
+    Directory.CreateDirectory(dir);
+}
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(dir),
+    RequestPath = "/images"
+});
 
 app.Run();
