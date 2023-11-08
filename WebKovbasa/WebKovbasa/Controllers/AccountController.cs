1f4fdaad48d3a451ab77b3133c14123de6add9b5
System Administrator <ivanivash>
2023-11-09 00:30:31 +0200

    Add Image Register

--- WebKovbasa/WebKovbasa/Controllers/AccountController.cs
+++ WebKovbasa/WebKovbasa/Controllers/AccountController.cs
@@ -1,38 +1,73 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebKovbasa.Constants;
 using WebKovbasa.Data.Entities.Identity;
+using WebKovbasa.Helpers;
+using WebKovbasa.Interfaces;
 using WebKovbasa.Models.Account;
 
-namespace WebKovbasa.Controllers;
-
-public class AccountController : Controller
+namespace WebKovbasa.Controllers
 {
-    private readonly UserManager<UserEntity> _userManager;
-    public AccountController(UserManager<UserEntity> userManager)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
     {
-        _userManager = userManager;
-    }
-    [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody] RegisterUserViewModel model)
-    {
-        UserEntity user = new UserEntity()
+        private readonly UserManager<UserEntity> _userManager;
+        private readonly IJwtTokenService _jwtTokenService;
+        public AccountController(UserManager<UserEntity> userManager,
+            IJwtTokenService jwtTokenService)
         {
-            FirstName = model.FirstName,
-            LastName = model.LastName,
-            UserName = model.Email,
-            Email = model.Email
-        };
-
-        var result = await _userManager.CreateAsync(user, model.Password);
-        if (result.Succeeded)
-        {
-            result = await _userManager.AddToRoleAsync(user, Roles.User);
-            return Ok();
+            _userManager = userManager;
+            _jwtTokenService = jwtTokenService;
         }
-        else
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
         {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                bool isPasswordValid = await _userManager.CheckPasswordAsync(user, model.Password);
+                if (!isPasswordValid)
+                {
+                    return BadRequest();
+                }
+                var token = await _jwtTokenService.CreateToken(user);
+                return Ok(new { token });
+            }
             return BadRequest();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserViewModel model)
+        {
+            string imageName = null;
+
+            if (!string.IsNullOrEmpty(model.ImageBase64))
+            {
+                imageName = ImageWorker.SaveImage(model.ImageBase64);
+            }
+
+            UserEntity user = new UserEntity()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                UserName = model.Email,
+                Email = model.Email,
+                Image = imageName,
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                result = await _userManager.AddToRoleAsync(user, Roles.User);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- WebKovbasa/WebKovbasa/Models/Account/RegisterUserViewModel.cs
+++ WebKovbasa/WebKovbasa/Models/Account/RegisterUserViewModel.cs
@@ -1,10 +1,12 @@
-namespace WebKovbasa.Models.Account;
-
-public class RegisterUserViewModel
+namespace WebKovbasa.Models.Account
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string ConfirmPassword { get; set; }
+    public class RegisterUserViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ImageBase64 { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
 }
